2629ba49d489ca5177c20a9b5149d628f2fadafe
vantuyen123 <68177953+vantuyen123@users.noreply.github.com>
2020-10-02 00:30:31 +0700

    android learn

--- app/src/main/java/com/example/demoandroid/Screen3.java
+++ app/src/main/java/com/example/demoandroid/Screen3.java
@@ -13,45 +13,41 @@ import android.widget.Toast;
 
 
 public class Screen3 extends AppCompatActivity {
-    private EditText username,password;
-    private Button login,register,back;
+    private EditText username, password;
+    private Button login, register, back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen3);
 
 
-
-        this.username = (EditText)this.findViewById(R.id.editTextUser);
-        this.password =(EditText)this.findViewById(R.id.editTextPass);
-        this.login =(Button)this.findViewById(R.id.btn_Login);
-        login.setOnClickListener(new Button.OnClickListener(){
+        this.username = (EditText) this.findViewById(R.id.editTextUser);
+        this.password = (EditText) this.findViewById(R.id.editTextPass);
+        this.login = (Button) this.findViewById(R.id.btn_Login);
+        login.setOnClickListener(new Button.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if(username.getText().toString().isEmpty() || password.getText().toString().isEmpty())
-                {
-                    Toast.makeText(getApplicationContext(),"Vui lòng nhập vào tài khoản và mật khẩu :))",Toast.LENGTH_LONG).show();
-                }
-                else{
-                        Intent resultScreen = new Intent(Screen3.this, ResultLogin.class);
-                        Screen3.this.startActivity(resultScreen);
+            public void onClick(View v) {
+                if (username.getText().toString().isEmpty() || password.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Vui lòng nhập vào tài khoản và mật khẩu :))", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent resultScreen = new Intent(Screen3.this, ResultLogin.class);
+                    Screen3.this.startActivity(resultScreen);
                 }
             }
         });
-        this.back = (Button)this.findViewById(R.id.btn_back);
-        back.setOnClickListener(new Button.OnClickListener(){
-           @Override
-           public void onClick(View v)
-           {
-               Screen3.this.finish();
-           }
-        });
-        this.register = (Button)this.findViewById(R.id.btn_Register);
-        register.setOnClickListener(new Button.OnClickListener(){
+        this.back = (Button) this.findViewById(R.id.btn_back);
+        back.setOnClickListener(new Button.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Intent registrationScreen = new Intent(Screen3.this,Screen4.class);
+            public void onClick(View v) {
+                Screen3.this.finish();
+            }
+        });
+        this.register = (Button) this.findViewById(R.id.btn_Register);
+        register.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registrationScreen = new Intent(Screen3.this, Screen4.class);
                 Screen3.this.startActivity(registrationScreen);
             }
         });
