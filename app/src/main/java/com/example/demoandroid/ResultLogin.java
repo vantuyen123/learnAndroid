a29a88a3594af92f48754803eb2ad34aa6a1c2ca
vantuyen123 <68177953+vantuyen123@users.noreply.github.com>
2020-10-02 00:23:36 +0700

    first Commit

---
+++ app/src/main/java/com/example/demoandroid/ResultLogin.java
@@ -0,0 +1,14 @@
+package com.example.demoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_login);
+    }
+}
