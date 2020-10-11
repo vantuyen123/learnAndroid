65d441fe78112c6317faa3d9cf6bbcb32d76591d
vantuyen123 <68177953+vantuyen123@users.noreply.github.com>
2020-10-12 03:45:08 +0700

    View holder learn

--- app/src/main/java/com/example/demoandroid/MainActivity.java
+++ app/src/main/java/com/example/demoandroid/MainActivity.java
@@ -3,14 +3,6 @@ package com.example.demoandroid;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
-
-import com.example.demoandroid.Post.PostItem;
-import com.example.demoandroid.adapter.PostAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,39 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView postRecyclerView = findViewById(R.id.postRecyclerView);
-        postRecyclerView.setLayoutManager(
-                new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
-        );
-        List<PostItem> postItems = new ArrayList<>();
-        postItems.add(new PostItem(R.drawable.agvae));
-        postItems.add(new PostItem(R.drawable.anh));
-        postItems.add(new PostItem(R.drawable.anhydep1));
-        postItems.add(new PostItem(R.drawable.anime1));
-        postItems.add(new PostItem(R.drawable.anime2));
-        postItems.add(new PostItem(R.drawable.anime3));
-        postItems.add(new PostItem(R.drawable.anime4));
-        postItems.add(new PostItem(R.drawable.anime5));
-        postItems.add(new PostItem(R.drawable.anime6));
-        postItems.add(new PostItem(R.drawable.anime7));
-        postItems.add(new PostItem(R.drawable.anime8));
-        postItems.add(new PostItem(R.drawable.anime9));
-        postItems.add(new PostItem(R.drawable.anime10));
-        postItems.add(new PostItem(R.drawable.anime11));
-        postItems.add(new PostItem(R.drawable.anime12));
-        postItems.add(new PostItem(R.drawable.anime13));
-        postItems.add(new PostItem(R.drawable.anime14));
-        postItems.add(new PostItem(R.drawable.anime15));
-        postItems.add(new PostItem(R.drawable.anime16));
-        postItems.add(new PostItem(R.drawable.anime17));
-        postItems.add(new PostItem(R.drawable.anime18));
-        postItems.add(new PostItem(R.drawable.anime19));
-        postItems.add(new PostItem(R.drawable.anime20));
-        postItems.add(new PostItem(R.drawable.b83651e87332d7a9e4328ab93fe785e4));
-        postItems.add(new PostItem(R.drawable.dc0eeb9e5380973bfff6a5d5930da2b3));
-        postItems.add(new PostItem(R.drawable.edqrer));
-        postItems.add(new PostItem(R.drawable.j5iyepy));
 
-        postRecyclerView.setAdapter(new PostAdapter(postItems));
     }
 }
